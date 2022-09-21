66f058e1f5bb2028277c18d32787f4b82956b68c
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-09-21 18:43:43 -0500

    Modified xpath

--- weather.py
+++ weather.py
@@ -18,9 +18,15 @@ location.click()
 
 time.sleep(3)
 
-by_hours = _Driver.find_element(By.XPATH, '/html/body/div[5]/div/div[4]/div/section[4]/section/div/article/section/ul/li[2]')
+by_hours = _Driver.find_element(By.XPATH, '/html/body/div[5]/div/div[4]/div/section[3]/section/div/article/section/ul[1]/li[2]')
 by_hours.click()
 
 time.sleep(3)
+
+txtcolumns  = _Driver.find_element(By.XPATH, '/html/body/div[5]/div/div[4]/div/section[3]/section/div[1]/ul').text
+data = txtcolumns.split('Hoy')[1].split("\n")
+
+for d in data:
+  print(data)
 
 _Driver.quit()
